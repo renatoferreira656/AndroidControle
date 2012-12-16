7789b642afac56fc9b1158495f474ad8383fcf32
Renato <Renato@Renatos-MacBook-Pro.local>
2012-12-16 21:59:10 -0200

    Validacao

---
+++ ControleProcessosLegais/src/app/controleprocessoslegais/util/Validador.java
@@ -0,0 +1,44 @@
+package app.controleprocessoslegais.util;
+
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class Validador {
+
+	 public static boolean validateNotNull(View pView, String pMessage) {
+		  if (pView instanceof EditText) {
+		   EditText edText = (EditText) pView;
+		   Editable text = edText.getText();
+		   if (text != null) {
+		    String strText = text.toString();
+		    if (!TextUtils.isEmpty(strText)) {
+		     return true;
+		    }
+		   }
+		   // em qualquer outra condição é gerado um erro
+		   edText.setError(pMessage);
+		   edText.setFocusable(true);
+		   edText.requestFocus();
+		   return false;
+		  }else if(pView instanceof Spinner){
+			   Spinner spinnerText = (Spinner) pView;
+			   Object text = spinnerText.getSelectedItem();
+			   if (text != null) {
+			    String strText = text.toString();
+			    if (!TextUtils.isEmpty(strText)) {
+			     return true;
+			    }
+			   }
+			   // em qualquer outra condição é gerado um erro
+//			   spinnerText.setsetError(pMessage);
+			   spinnerText.setFocusable(true);
+			   spinnerText.requestFocus();
+			   return false;	  
+		  }
+		  return false;
+		 }
+	
+}
