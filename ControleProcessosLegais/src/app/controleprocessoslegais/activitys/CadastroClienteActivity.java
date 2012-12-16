2c268f495c8b1f70ed20f0fa725af091b731960e
Renato <Renato@Renatos-MacBook-Pro.local>
2012-12-16 21:59:37 -0200

    Validacao

--- ControleProcessosLegais/src/app/controleprocessoslegais/activitys/CadastroClienteActivity.java
+++ ControleProcessosLegais/src/app/controleprocessoslegais/activitys/CadastroClienteActivity.java
@@ -1,28 +1,34 @@
 package app.controleprocessoslegais.activitys;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import app.controleprocessoslegais.R;
-import app.controleprocessoslegais.util.Tags;
+import app.controleprocessoslegais.dao.ClienteDao;
+import app.controleprocessoslegais.util.Builders;
+import app.controleprocessoslegais.wrappers.ClienteWrapper;
 
 public class CadastroClienteActivity extends Activity {
-	Map<Tags,String> mapaSpinner = new HashMap<Tags,String>();
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cadastro_cliente);
-		// Button botaoSalvar = (Button) findViewById(R.id.botaoSalvar);
-		// final ClienteDao dao = new ClienteDao(getApplicationContext());
-		// final CadastroClienteActivity activity = this;
-		// botaoSalvar.setOnClickListener(new View.OnClickListener() {
-		// public void onClick(View v) {
-		// ClienteWrapper clienteNovo = Builders
-		// .constroiClienteFromViews(activity);
-		// dao.addCliente(clienteNovo);
-		// }
-		// });
+		inicializaBotaoSalvar();
+	}
+
+	private final void inicializaBotaoSalvar() {
+		Button botaoSalvar = (Button) findViewById(R.id.botaoSalvar);
+		final ClienteDao dao = new ClienteDao(getApplicationContext());
+		final CadastroClienteActivity activity = this;
+		botaoSalvar.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				ClienteWrapper clienteNovo = Builders.constroiClienteFromViews(activity);
+				if (clienteNovo != null) {
+					dao.addCliente(clienteNovo);
+				}
+			}
+		});
 	}
 }
--- ControleProcessosLegais/src/app/controleprocessoslegais/util/Builders.java
+++ ControleProcessosLegais/src/app/controleprocessoslegais/util/Builders.java
@@ -1,5 +1,11 @@
 package app.controleprocessoslegais.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.res.Resources;
+import android.text.Editable;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import app.controleprocessoslegais.R;
@@ -9,44 +15,90 @@ import app.controleprocessoslegais.wrappers.ClienteWrapper;
 public class Builders {
 
 	public static ClienteWrapper constroiClienteFromViews(CadastroClienteActivity activityCliente) {
-		EditText bairroEditView = (EditText) activityCliente.findViewById(R.id.bairroEditView);
-		EditText cepEditView = (EditText) activityCliente.findViewById(R.id.cepEditView);
+		List<View> listaCamposObrigatorio = new ArrayList<View>();
 		EditText nomeEditView = (EditText) activityCliente.findViewById(R.id.nomeEditView);
+		listaCamposObrigatorio.add(nomeEditView);
 		EditText rgEditView = (EditText) activityCliente.findViewById(R.id.rgEditView);
-		EditText cpfCnpjEditView = (EditText) activityCliente.findViewById(R.id.cpfcnpjEditView);
+		listaCamposObrigatorio.add(rgEditView);
 		EditText orgaoExpedidorEditView = (EditText) activityCliente.findViewById(R.id.orgaoExpedidorEditView);
+		listaCamposObrigatorio.add(orgaoExpedidorEditView);
+		EditText cpfCnpjEditView = (EditText) activityCliente.findViewById(R.id.cpfcnpjEditView);
+		listaCamposObrigatorio.add(cpfCnpjEditView);
 		EditText ruaEditView = (EditText) activityCliente.findViewById(R.id.ruaEditView);
+		listaCamposObrigatorio.add(ruaEditView);
+		EditText bairroEditView = (EditText) activityCliente.findViewById(R.id.bairroEditView);
+		listaCamposObrigatorio.add(bairroEditView);
+		EditText cepEditView = (EditText) activityCliente.findViewById(R.id.cepEditView);
+		listaCamposObrigatorio.add(cepEditView);
+		Spinner estadoSpinnerView = (Spinner) activityCliente.findViewById(R.id.estadoSpinnerView);
+		listaCamposObrigatorio.add(estadoSpinnerView);
 		EditText numeroEditView = (EditText) activityCliente.findViewById(R.id.numeroEditView);
+		listaCamposObrigatorio.add(numeroEditView);
 		Spinner estadoCivilEditView = (Spinner) activityCliente.findViewById(R.id.estadoCivilSpinnerView);
+		listaCamposObrigatorio.add(estadoCivilEditView);
 		EditText nacionalidadeEditView = (EditText) activityCliente.findViewById(R.id.nacionalidadeEditView);
+		listaCamposObrigatorio.add(nacionalidadeEditView);
 		EditText profissaoEditView = (EditText) activityCliente.findViewById(R.id.profissaoEditView);
+		listaCamposObrigatorio.add(profissaoEditView);
+
+		if (camposObrigatoriosClienteSaoValidos(listaCamposObrigatorio, activityCliente.getResources())) {
+			return null;
+		}
+
 		EditText maeEditView = (EditText) activityCliente.findViewById(R.id.maeEditView);
 		EditText paiEditView = (EditText) activityCliente.findViewById(R.id.paiEditView);
 		EditText ctpsEditView = (EditText) activityCliente.findViewById(R.id.ctpsEditView);
 		EditText pisPasepEditView = (EditText) activityCliente.findViewById(R.id.pisPasepEditView);
 		EditText inscricaoEstadualEditView = (EditText) activityCliente.findViewById(R.id.inscricaoEstadualEditView);
-		Spinner estadoSpinnerView = (Spinner) activityCliente.findViewById(R.id.estadoSpinnerView);
 
 		ClienteWrapper cliente = new ClienteWrapper();
 		cliente.setBairro(bairroEditView.getText().toString());
 		cliente.setCep(cepEditView.getText().toString());
 		cliente.setCnpg(cpfCnpjEditView.getText().toString());
 		cliente.setCpf(cpfCnpjEditView.getText().toString());
-		cliente.setCtps(ctpsEditView.getText().toString());
 		cliente.setEstado(estadoSpinnerView.getSelectedItem().toString());
 		cliente.setEstadoCivil(estadoCivilEditView.getSelectedItem().toString());
-		cliente.setInscricaoEstadual(inscricaoEstadualEditView.getText().toString());
 		cliente.setNacionalidade(nacionalidadeEditView.getText().toString());
 		cliente.setNomeCompleto(nomeEditView.getText().toString());
 		cliente.setNumero(numeroEditView.getText().toString());
 		cliente.setOrgaoExpedidor(orgaoExpedidorEditView.getText().toString());
-		cliente.setPisPasep(pisPasepEditView.getText().toString());
 		cliente.setProfissao(profissaoEditView.getText().toString());
 		cliente.setRg(rgEditView.getText().toString());
 		cliente.setRua(ruaEditView.getText().toString());
-		cliente.setPai(paiEditView.getText().toString());
-		cliente.setMae(maeEditView.getText().toString());
+
+		Editable inscricaoEstadual = inscricaoEstadualEditView.getText();
+		if (inscricaoEstadual != null) {
+			cliente.setInscricaoEstadual(inscricaoEstadual.toString());
+		}
+		Editable pisPasep = pisPasepEditView.getText();
+		if (pisPasep != null) {
+			cliente.setPisPasep(pisPasep.toString());
+		}
+
+		Editable ctps = ctpsEditView.getText();
+		if (ctps != null) {
+			cliente.setCtps(ctps.toString());
+		}
+		Editable pai = paiEditView.getText();
+
+		if (pai != null) {
+			cliente.setPai(pai.toString());
+		}
+		Editable mae = maeEditView.getText();
+
+		if (mae != null) {
+			cliente.setMae(mae.toString());
+		}
 
 		return cliente;
+	}
+
+	private static boolean camposObrigatoriosClienteSaoValidos(List<View> listaCampos, Resources resources) {
+		for (View view : listaCampos) {
+			if (!Validador.validateNotNull(view, resources.getString(R.string.campo_obrigatorio))) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
